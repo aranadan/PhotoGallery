15cc5e7972f01a9770b3a74af9568b6e32bbc13d
aranadan <aranadan@gmail.com>
2018-03-26 22:44:07 +0300

    //dynamic config items on screen, add cache

--- app/src/main/java/com/fox/andrey/photogallery/PhotoGalleryFragment.java
+++ app/src/main/java/com/fox/andrey/photogallery/PhotoGalleryFragment.java
@@ -1,6 +1,7 @@
 package com.fox.andrey.photogallery;
 
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.AsyncTask;
@@ -17,6 +18,7 @@ import android.view.ViewGroup;
 import android.view.ViewTreeObserver;
 import android.widget.ImageView;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -30,7 +32,6 @@ public class PhotoGalleryFragment extends Fragment {
     private static int page = 1;
 
 
-
     public static PhotoGalleryFragment newInstance() {
         return newInstance();
     }
@@ -38,10 +39,11 @@ public class PhotoGalleryFragment extends Fragment {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Log.i(TAG, "Fragment onCreate");
-        //удерживаю фрагмент
+        Log.i(TAG, "onCreate ");
+        //hold fragment
         setRetainInstance(true);
-        //выполняю загрузку данных в фоне
+
+        //get data in background
         new FetchItemTask().execute();
 
         Handler responseHandler = new Handler();
@@ -58,10 +60,11 @@ public class PhotoGalleryFragment extends Fragment {
         Log.i(TAG, "Background thread started");
     }
 
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-         final View v = inflater.inflate(R.layout.fragment_photo_gallery, container, false);
+        final View v = inflater.inflate(R.layout.fragment_photo_gallery, container, false);
         mPhotoRecyclerView = v.findViewById(R.id.photo_recycler_view);
         //динамическое обновление кол-ва колонок в зависимости от ширины дисплея
         mPhotoRecyclerView.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
@@ -70,7 +73,7 @@ public class PhotoGalleryFragment extends Fragment {
                 int spanCount = 2;
                 int width = getView().getWidth();
                 if (width > 1000) spanCount = 3;
-                else if (width > 1800) spanCount =4;
+                if (width > 1600) spanCount = 4;
                 mPhotoRecyclerView.getViewTreeObserver().removeOnGlobalLayoutListener(this);
                 mPhotoRecyclerView.setLayoutManager(new GridLayoutManager(getActivity(), spanCount));
             }
@@ -78,11 +81,16 @@ public class PhotoGalleryFragment extends Fragment {
 
         //прослушка для добавления новой партии фотографий при достижении последнего пункта списка
         mPhotoRecyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+
+
+
             @Override
             public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                GridLayoutManager layoutManager = ((GridLayoutManager) mPhotoRecyclerView.getLayoutManager());
                 super.onScrolled(recyclerView, dx, dy);
-                //если последняя строчка списка
-                if (!mPhotoRecyclerView.canScrollVertically(1) && loadingAvailable) {
+                //если сумма последней видимой позиции и всех видимых позиций больше общего количества позиций, то гружу следующую страницу
+                if (layoutManager.findLastVisibleItemPosition() + layoutManager.getChildCount() >= layoutManager.getItemCount()
+                        && loadingAvailable) {
                     loadingAvailable = false;
                     Log.i(TAG, "Last item of list");
                     pageIncrement();
@@ -90,6 +98,8 @@ public class PhotoGalleryFragment extends Fragment {
                     new FetchItemTask().execute();
                 }
             }
+
+
         });
 
         setupAdapter();
@@ -103,14 +113,11 @@ public class PhotoGalleryFragment extends Fragment {
 
     }
 
+
     private void setupAdapter() {
         //если фрагмент присоединен к активности
         if (isAdded()) {
-
-            Log.i(TAG, "adapter status is:" + mPhotoRecyclerView.getAdapter());
             mPhotoRecyclerView.setAdapter(new PhotoAdapter(mItems));
-            Log.i(TAG, "new adapter was created");
-            //}else mPhotoRecyclerView.getAdapter().notifyDataSetChanged();
         }
     }
 
@@ -146,7 +153,6 @@ public class PhotoGalleryFragment extends Fragment {
 
         @Override
         public void onBindViewHolder(PhotoHolder holder, int position) {
-            Log.i(TAG, "Position is:" + position);
             GalleryItem item = mGalleryItem.get(position);
             Drawable placeHolder = getResources().getDrawable(R.mipmap.no_image);
             holder.bindDrawable(placeHolder);
@@ -168,9 +174,13 @@ public class PhotoGalleryFragment extends Fragment {
 
         @Override
         protected void onPostExecute(List<GalleryItem> galleryItems) {
-            mItems.addAll(galleryItems);
-            //setupAdapter();
-            mPhotoRecyclerView.getAdapter().notifyDataSetChanged();
+            for (int i = 0; i < galleryItems.size(); i++){
+                GalleryItem item = galleryItems.get(i);
+                mItems.add(item);
+                mPhotoRecyclerView.getAdapter().notifyItemChanged(mItems.size()-1);
+                new Thread(() -> downloadToCache(item.getmURL())).start();
+            }
+
             loadingAvailable = true;
         }
     }
@@ -189,5 +199,21 @@ public class PhotoGalleryFragment extends Fragment {
 
     private String getPage() {
         return String.valueOf(page);
+    }
+
+    private void downloadToCache (String url){
+        try {
+            if (url == null) {
+                return;
+            }
+            if (Cache.getInstance().getLru().get(url) == null) {
+                byte[] bitmapBytes = new FlickrFetchr().getUrlBytes(url);
+                Bitmap bitmap = BitmapFactory.decodeByteArray(bitmapBytes, 0, bitmapBytes.length);
+                Cache.getInstance().getLru().put(url, bitmap);
+                Log.i(TAG, "Download to cache");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- app/src/main/java/com/fox/andrey/photogallery/ThumbnailDownloader.java
+++ app/src/main/java/com/fox/andrey/photogallery/ThumbnailDownloader.java
@@ -23,20 +23,19 @@ public class ThumbnailDownloader<T> extends HandlerThread {
 
     private boolean mHasQuit = false;
     private Handler mRequestHandler;
-    private ConcurrentMap<T,String> mRequestMap = new ConcurrentHashMap<>();
+    private ConcurrentMap<T, String> mRequestMap = new ConcurrentHashMap<>();
     private Handler mResponseHandler;
     private ThumbnailDownloaderListener<T> mThumbnailDownloaderListener;
 
-    public interface ThumbnailDownloaderListener<T>{
-       void onThumbnailDownloaded(T target, Bitmap thumbnail);
+    public interface ThumbnailDownloaderListener<T> {
+        void onThumbnailDownloaded(T target, Bitmap thumbnail);
     }
 
-    public void setThumbnailDownloaderListener(ThumbnailDownloaderListener<T> listener){
+    public void setThumbnailDownloaderListener(ThumbnailDownloaderListener<T> listener) {
         mThumbnailDownloaderListener = listener;
     }
 
     public ThumbnailDownloader(Handler responseHandler) {
-
         super(TAG);
         mResponseHandler = responseHandler;
     }
@@ -48,20 +47,19 @@ public class ThumbnailDownloader<T> extends HandlerThread {
     }
 
 
-
     public void queueThumbnail(T target, String url) {
         //Log.i(TAG, "Got a URL: " + url);
 
-        if(url == null){
+        if (url == null) {
             mRequestMap.remove(target);
-        }else {
-            mRequestMap.put(target,url);
+        } else {
+            mRequestMap.put(target, url);
             //извлекаю сообщение из глобального пула сообщений
-            mRequestHandler.obtainMessage(MESSAGE_DOWNLOAD,target).sendToTarget();
+            mRequestHandler.obtainMessage(MESSAGE_DOWNLOAD, target).sendToTarget();
         }
     }
 
-    public void clearQueue(){
+    public void clearQueue() {
         mRequestHandler.removeMessages(MESSAGE_DOWNLOAD);
         mRequestMap.clear();
     }
@@ -69,37 +67,46 @@ public class ThumbnailDownloader<T> extends HandlerThread {
     @Override
     protected void onLooperPrepared() {
         super.onLooperPrepared();
-        mRequestHandler = new Handler(){
+        mRequestHandler = new Handler() {
             @Override
             public void handleMessage(Message msg) {
-                if(msg.what == MESSAGE_DOWNLOAD){
+
+                if (msg.what == MESSAGE_DOWNLOAD) {
                     T target = (T) msg.obj;
-                    //Log.i(TAG,"Got a request for URL: " + mRequestMap.get(target));
+                    //Log.i(TAG, "Got a request for message download: " + mRequestMap.get(target));
                     handleRequest(target);
                 }
-                        }
+            }
         };
     }
 
     private void handleRequest(final T target) {
-        try{
+
+        try {
             final String url = mRequestMap.get(target);
-            if (url == null){
+            final Bitmap bitmap;
+            if (url == null) {
                 return;
             }
-
-            byte[] bitmapBytes = new FlickrFetchr().getUrlBytes(url);
-            final Bitmap bitmap = BitmapFactory.decodeByteArray(bitmapBytes, 0, bitmapBytes.length);
-
+            if (Cache.getInstance().getLru().get(url) == null) {
+                byte[] bitmapBytes = new FlickrFetchr().getUrlBytes(url);
+                bitmap = BitmapFactory.decodeByteArray(bitmapBytes, 0, bitmapBytes.length);
+                Cache.getInstance().getLru().put(url, bitmap);
+                Log.i(TAG, "Download from internet");
+            }else{
+                bitmap = Cache.getInstance().getLru().get(url);
+                Log.i(TAG, "Download from cache");
+            }
+            //run new thread in UI because handler create in main thread
             mResponseHandler.post(new Runnable() {
                 @Override
                 public void run() {
 
-                    if (mRequestMap.get(target) != url || mHasQuit){
+                    if (mRequestMap.get(target) != url || mHasQuit) {
                         return;
                     }
-
                     mRequestMap.remove(target);
+                    //send objects to PhotoGalleryFragment
                     mThumbnailDownloaderListener.onThumbnailDownloaded(target, bitmap);
                 }
             });
